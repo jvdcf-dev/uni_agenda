4e1f322a32d42433be62d066acfbd2c9c8326576
jvdcf <up202208393@up.pt>
2023-07-27 16:09:23 +0100

    Assessment tests written

--- test.cpp
+++ test.cpp
@@ -22,7 +22,7 @@ void Date_tests() {
     try {
         {Date d = Date(1753, 0, 1, 48, 0, 0);
         cout << d.str();}
-    } catch(char const* e) {}
+    } catch(const char* e) {}
 
     {Date d1 = Date(1900, 1, 1, 0, 0, 0);
     Date d2 = Date(1900, 1, 1, 1, 1, 1);
@@ -41,8 +41,61 @@ void Date_tests() {
     assert(hms.hours() == std::chrono::hours(15));}
 }
 
+
 void time_tests() {}
-void assessment_tests() {}
+
+
+void assessment_tests() {
+    {Assessment a = Assessment();
+    assert(a.get_name() == "N/A");
+    assert(a.get_weight() == 0);
+    assert(a.get_date().year() == std::chrono::year(1900));
+    assert(a.get_date().month() == std::chrono::month(1));
+    assert(a.get_date().day() == std::chrono::day(1));
+    assert(a.get_grade() == 0);}
+
+    {Assessment a = Assessment("Test", 0.5, 2020, 9, 20, 15.2);
+    assert(a.get_name() == "Test");
+    assert(a.get_weight() == 0.5);
+    assert(a.get_date().year() == std::chrono::year(2020));
+    assert(a.get_date().month() == std::chrono::month(9));
+    assert(a.get_date().day() == std::chrono::day(20));
+    assert(a.get_grade() == 15.2);}
+
+    {Assessment a = Assessment("Another test", 0.9, 2020, 11, 23, 18.4);
+    a.set_name("Just a test");
+    a.set_weight(0.4);
+    a.set_date(2022, 12, 26);
+    a.set_grade(17.5);
+    assert(a.get_name() == "Just a test");
+    assert(a.get_weight() == 0.4);
+    assert(a.get_date().year() == std::chrono::year(2022));
+    assert(a.get_date().month() == std::chrono::month(12));
+    assert(a.get_date().day() == std::chrono::day(26));
+    assert(a.get_grade() == 17.5);}
+
+    try {
+        {Assessment a = Assessment();
+        a.set_grade(-13.4);}
+    } catch(const char* e) {}
+
+    try {
+        {Assessment a = Assessment();
+        a.set_weight(1.2);}
+    } catch(const char* e) {}
+
+    try {
+        {Assessment a = Assessment();
+        a.set_grade(23.25);}
+    } catch(const char* e) {}
+
+    try {
+        {Assessment a = Assessment();
+        a.set_weight(-0.1);}
+    } catch(const char* e) {}
+}
+
+
 void task_tests() {}
 void summary_tests() {}
 void subject_tests() {}
